621a9fd06f5651042cf22600dbd9d69389be456f
Alvin Kim <alvinkim101@gmail.com>
2025-06-16 22:15:40 -0700

    1. Vulkan Initialization Code

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -7,13 +7,25 @@
 
 class VulkanEngine {
 public:
-
 	bool _isInitialized{ false };
 	int _frameNumber {0};
 	bool stop_rendering{ false };
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
 	struct SDL_Window* _window{ nullptr };
+
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
 
 	static VulkanEngine& Get();
 
@@ -28,4 +40,13 @@ public:
 
 	//run main loop
 	void run();
+
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
